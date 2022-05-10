8cdadc5942dae198028c8d17af0af91977d3f9df
zhangruize <zhangruize.zrz@antgroup.com>
2022-05-11 00:43:42 +0800

    update

--- README.md
+++ README.md
@@ -1,37 +1,38 @@
-logdog 是一个试图简化日志分析的工具。
+Parse input lines using custom patterns and output more readable and helpful lines.
 
 ## Install
 
-`tnpm install -g logdog`
+`npm install -g linecat`
 
 ## Usage
 
-- `logdog patterns` 查看所有的 pattern。
-- `logdog yourlog -p yourpattern`
+- `linecat -p=pattern_path`
 
-## Pattern
+pattern_path is the custom pattern json file. Example patterns json files are listed in src/example-patterns.
 
-| 字段         | 必填/可选 | 说明                                         |
-| ------------ | --------- | -------------------------------------------- |
-| id           | 可选      | 标识该 Pattern，可在`outputFormat`中间接引用 |
-| line         | 可选      | 对整行进行正则匹配                           |
-| comment      | 可选      | 备注说明，可在`outputFormat`中间接引用       |
-| outputFormat | 可选      | 输出格式，默认`${line}`。                    |
+## Pattern json file
 
-其中`outputFormat`配置项可以引用一些环境变量，包括：
+| field        | require | note                                                                       |
+| ------------ | ------- | -------------------------------------------------------------------------- |
+| id           | no      | the id of current pattern, which can be referenced in `outputFormat` field |
+| line         | no      | regular expression for whole line content                                  |
+| outputFormat | no      | specific output line format. Available env variables are listed below.     |
 
-| 环境变量               | 说明                                                                       |
-| ---------------------- | -------------------------------------------------------------------------- |
-| id                     | 该 Pattern 的 id                                                           |
-| line                   | 整行内容                                                                   |
-| content                | 除了日志日期、标签、级别的行内容                                           |
-| tag                    | 日志标签（不建议依赖）                                                     |
-| level                  | 日志级别（不建议依赖）                                                     |
-| month                  | 日志中的月份                                                               |
-| day                    | 日志中的日                                                                 |
-| hours                  | 日志中的小时                                                               |
-| minutes                | 日志中的分钟                                                               |
-| seconds                | 日志中的秒                                                                 |
-| ms                     | 日志中的毫秒                                                               |
-| timediff               | 和当前行的日期差(取反），单位 ms                                           |
-| last-\[id\|line]-{...} | 查询上一个指定 id 匹配项，或查询上一行的上述字段。比如：last-line-timediff |
+Many env variables are available in `outputFormat` field:
+
+| Env var name           | note                                                                                            |
+| ---------------------- | ----------------------------------------------------------------------------------------------- |
+| id                     | current pattern id                                                                              |
+| line                   | whole line                                                                                      |
+| content                | whole line except other tokens                                                                  |
+| level                  | log level if available                                                                          |
+| month                  | month of current line if available                                                              |
+| day                    | date of current line if available                                                               |
+| hours                  | hours of current line if available                                                              |
+| minutes                | minute of current line if available                                                             |
+| seconds                | seconds of current line if available                                                            |
+| ms                     | ms of current line if avaiilable                                                                |
+| timediff               | time diff from current line.                                                                    |
+| last-\[id\|line]-{...} | last matched line of specific id or any matched line for fields above. eg: `last-line-timediff` |
+
+You can also check example patterns in `src/example-patterns` to know more usage.
--- build/index.js
+++ build/index.js
@@ -1,117 +1,100 @@
-import * as fs from 'fs';
-import * as path from 'path';
-import * as process from 'process';
-import { exit } from 'process';
-import * as readline from 'readline';
-
-export type SettingsFile = {
-    varPatterns: {
-        id: string;
-        regex: string;
-    }[];
-    linePatterns: {
-        inputFormat: string;
-        regex?: RegExp;
-    }[];
-    defaultOutputFormat: string;
-    autoInsertLineBreak: boolean;
-    warnings: {
-        unrecoginizedLine: boolean;
-    };
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
 };
-
-export type PatternsFile = {
-    patterns: {
-        id?: string;
-        line: string;
-        comment?: string;
-        outputFormat?: string;
-        regex?: RegExp;
-    }[];
-};
-
-
-let inject = (str: string, obj: any) => str.replace(/\${(.*?)}/g, (x, g) => obj[g]);
-
-function prepareSettings(settingsFile: SettingsFile) {
-    let varEnv: { [index: string]: string; } = {};
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs = __importStar(require("fs"));
+const path = __importStar(require("path"));
+const process = __importStar(require("process"));
+const process_1 = require("process");
+const readline = __importStar(require("readline"));
+let inject = (str, obj) => str.replace(/\${(.*?)}/g, (x, g) => obj[g]);
+function prepareSettings(settingsFile) {
+    let varEnv = {};
     settingsFile.varPatterns.forEach(vp => {
         varEnv[vp.id] = `(${vp.regex})`;
     });
-    settingsFile.linePatterns.forEach((lp: any) => {
+    settingsFile.linePatterns.forEach((lp) => {
         lp.regex = new RegExp(inject(lp.inputFormat, varEnv));
     });
     return settingsFile;
 }
-
-function preparePatterns(patternsFile: PatternsFile) {
+function preparePatterns(patternsFile) {
     patternsFile.patterns.forEach(p => {
         if (p.line) {
             p.regex = new RegExp(p.line);
-        } else {
+        }
+        else {
             console.warn("Not a valid pattern:", p);
         }
     });
 }
-
-let settings = JSON.parse(fs.readFileSync(path.resolve(__dirname, "settings.json"), { encoding: "utf-8" })) as SettingsFile;
+let settings = JSON.parse(fs.readFileSync(path.resolve(__dirname, "settings.json"), { encoding: "utf-8" }));
 prepareSettings(settings);
-
-let patternFilePath: string | undefined = undefined;
+let patternFilePath = undefined;
 process.argv.forEach(function (val) {
     if (val.startsWith("-p=")) {
         patternFilePath = val.substring(val.indexOf('=') + 1);
     }
 });
-
 if (!patternFilePath) {
     console.error("No valid pattern file. Please specific pattern file by: '-p=your_pattern.json'");
-    exit();
+    (0, process_1.exit)();
 }
-
-let userPatterns = JSON.parse(fs.readFileSync(path.resolve("./", patternFilePath), { encoding: "utf-8" })) as PatternsFile;
+let userPatterns = JSON.parse(fs.readFileSync(path.resolve("./", patternFilePath), { encoding: "utf-8" }));
 preparePatterns(userPatterns);
 if (!userPatterns.patterns || !Array.isArray(userPatterns.patterns) || userPatterns.patterns.length <= 0) {
     console.error("No valid pattern file. Please specific pattern file by: '-p=your_pattern.json'");
-    exit();
+    (0, process_1.exit)();
 }
-
 let input = process.stdin;
 let output = process.stdout;
-
 let rl = readline.createInterface({
     input,
     output,
 });
-
-let env: { [index: string]: any; } = {};
+let env = {};
 rl.on("line", line => {
-    let lineEnv: { [index: string]: any; } = { line };
+    let lineEnv = { line };
     let matchedPattern = settings.linePatterns.find(linePattern => {
         if (linePattern.regex && linePattern.regex.test(line)) {
             let results = linePattern.regex.exec(line);
             if (results != null) {
                 let i = 1;
-                linePattern.inputFormat.replace(/\${(.*?)}/g, (x, g) => i < results!.length ? lineEnv[g] = results![i++] : "");
-                lineEnv.time = new Date(2000, parseInt(lineEnv['month']), parseInt(lineEnv['day']), parseInt(lineEnv['hours']),
-                    parseInt(lineEnv['minutes']), parseInt(lineEnv['seconds']), parseInt(lineEnv['ms']));
+                linePattern.inputFormat.replace(/\${(.*?)}/g, (x, g) => i < results.length ? lineEnv[g] = results[i++] : "");
+                lineEnv.time = new Date(2000, parseInt(lineEnv['month']), parseInt(lineEnv['day']), parseInt(lineEnv['hours']), parseInt(lineEnv['minutes']), parseInt(lineEnv['seconds']), parseInt(lineEnv['ms']));
                 return true;
             }
         }
     });
-
-    let matchedUserPatternId: string | undefined = undefined;
+    let matchedUserPatternId = undefined;
     if (matchedPattern) {
         let matchedUserPattern = userPatterns.patterns.find(p => {
             if (p.regex && p.regex.test(line)) {
                 // prepare user pattern var env.
-                let userEnv: any = {};
+                let userEnv = {};
                 if (p.comment) {
                     userEnv.comment = p.comment;
                 }
                 Object.entries(env).forEach(entry => {
                     if (entry[0].endsWith("-time")) {
-                        (userEnv as any)[entry[0] + 'diff'] = lineEnv['time'] - entry[1];
+                        userEnv[entry[0] + 'diff'] = lineEnv['time'] - entry[1];
                     }
                 });
                 let outputFormat = p.outputFormat ? p.outputFormat : settings.defaultOutputFormat;
@@ -122,10 +105,8 @@ rl.on("line", line => {
                 return true;
             }
         });
-
         if (matchedUserPattern) {
             matchedUserPatternId = matchedUserPattern.id;
-
             Object.entries(lineEnv).forEach(entry => {
                 env[`last-line-${entry[0]}`] = entry[1];
                 if (matchedUserPatternId) {
@@ -133,7 +114,8 @@ rl.on("line", line => {
                 }
             });
         }
-    } else {
+    }
+    else {
         if (settings.warnings.unrecoginizedLine) {
             console.warn(`No pattern matched for line: ${line}`);
         }
